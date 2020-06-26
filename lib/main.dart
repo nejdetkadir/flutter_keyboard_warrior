b73f1e49ef2cef615dd494a55e60fcd3d7d4aaea
nejdetkadir <nejdetkadir.550@gmail.com>
2020-06-27 01:25:31 +0300

    Create second step

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-
+import 'package:marquee/marquee.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,13 +29,28 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int step = 0;
+  int score = 0;
+  var sentence = "                                                    Bir berber bir berbere bre berber gel beraber berberistanda bir berber dükkânı açalım diye bar bar bağırmış Paşa tasıyla taşa taşa beş tas has üzüm hoşafı Paşanın tasası taşanın maşası pas tutmaz şaşılır makası".toLowerCase();
   TextEditingController userName = TextEditingController();
+
+  void onWrote(String value) {
+    if (sentence.trimLeft()[value.length-1] == value[value.length-1] && value.length > 0) {
+      if (sentence.trimLeft()[value.length-1] != " ") {
+        score++;
+      }
+    } else {
+      setState(() {
+        step++;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     var shownWidget;
 
     if (step == 0) {
+      /*--------- STEP ZERO ---------*/
       shownWidget = <Widget>[
         Container(
           padding: EdgeInsets.only(right: 25, left: 25),
@@ -60,6 +77,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     textColor: Colors.white,
                     fontSize: 15.0
                 );
+              } else {
+                setState(() {
+                  step++;
+                });
               }
             },
             color: Colors.cyan,
@@ -70,12 +91,52 @@ class _MyHomePageState extends State<MyHomePage> {
         )
       ];
     } else if (step == 1) {
+      /*--------- STEP ONE ---------*/
+      int second = 0;
+      var timer = Timer.periodic(new Duration(seconds: 1), (timer) async {
+          second++;
+          if (second > 10 && score < 3) {
+            timer.cancel();
+            setState(() {
+              step++;
+            });
+          }
+      });
+
       shownWidget = <Widget>[
-        Text("step 1")
+        Container(
+          alignment: Alignment.center,
+          height: 100,
+          child: Marquee(
+            text: sentence,
+            style: TextStyle(fontSize: 24, letterSpacing: 2),
+            scrollAxis: Axis.horizontal,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            blankSpace: 20.0,
+            velocity: 125,
+            startPadding: 0,
+            accelerationDuration: Duration(seconds: 15),
+            accelerationCurve: Curves.ease,
+            decelerationDuration: Duration(milliseconds: 500),
+            decelerationCurve: Curves.easeOut,
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.only(right: 25, left: 25),
+          child: TextField(
+            autofocus: true,
+            onChanged: onWrote,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "GO GO GO!",
+            ),
+          ),
+        ),
       ];
     } else {
       shownWidget = <Widget>[
-        Text("step 2")
+        Text("score : ${score}")
       ];
     }
 
@@ -85,11 +146,7 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Expanded(
-              flex: 1,
-              child: Container(),
-            ),
-            Expanded(
-              flex: 10,
+              flex: 9,
               child: Container(
                 width: double.infinity,
                 decoration: BoxDecoration(
