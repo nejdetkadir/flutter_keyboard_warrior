d8ec5c6e6a9ca00a79c2c2805619a56ee10e1278
nejdetkadir <nejdetkadir.550@gmail.com>
2020-06-27 00:01:15 +0300

    Create step one of game

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -24,23 +26,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int step = 0;
+  TextEditingController userName = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    var shownWidget;
+
+    if (step == 0) {
+      shownWidget = <Widget>[
+        Container(
+          padding: EdgeInsets.only(right: 25, left: 25),
+          child: TextField(
+            controller: userName,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "What's your name?",
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.only(right: 25, left: 25),
+          margin: EdgeInsets.only(top: 20),
+          child: RaisedButton(
+            onPressed: () {
+              if (userName.text.toString().length < 2) {
+                Fluttertoast.showToast(
+                    msg: "It must be of 3 characters minimum",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.BOTTOM,
+                    timeInSecForIosWeb: 1,
+                    backgroundColor: Colors.red,
+                    textColor: Colors.white,
+                    fontSize: 15.0
+                );
+              }
+            },
+            color: Colors.cyan,
+            child: Text(
+              "START",style: TextStyle(color: Colors.white),
+            ),
+          ),
+        )
+      ];
+    } else if (step == 1) {
+      shownWidget = <Widget>[
+        Text("step 1")
+      ];
+    } else {
+      shownWidget = <Widget>[
+        Text("step 2")
+      ];
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Title"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        backgroundColor: Colors.cyan,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(
-              "Body"
+            Expanded(
+              flex: 1,
+              child: Container(),
+            ),
+            Expanded(
+              flex: 10,
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius:
+                  BorderRadius.only(bottomLeft: Radius.circular(80.0), topRight: Radius.circular(80.0)),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: shownWidget,
+                )
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(),
             )
           ],
-        ),
-      ),
-    );
+        ));
   }
 }
